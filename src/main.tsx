5a8b1fc5dc315d681df2a80090b3897af17ef4a0
s12hubha <s12hubham.pandey@gmail.com>
2024-06-23 19:26:00 +0530

    nw-chagnes

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./redux/store/strore.ts";
 import "react-toastify/dist/ReactToastify.css";
-
+/* eslint no-use-before-define: 0 */
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <App />
