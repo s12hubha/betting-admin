8662800783cae23289a6b626465d1227133b0b47
s12hubha <s12hubham.pandey@gmail.com>
2024-06-23 20:05:11 +0530

    nw-changes

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 import { AuthRoutes } from "../pages/auth/routes";
 import AuthService from "../core/service/auth.service";
 const auth= AuthService.isLoggedIn()
-
+// C.C
 export const publicRoutes=[
     {
     path:"/auth/*",
